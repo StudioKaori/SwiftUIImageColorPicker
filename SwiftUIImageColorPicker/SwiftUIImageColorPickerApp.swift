7fbfaadf68184cf30d35fc492feb7fd872cde19e
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-11-19 12:51:10 +0100

    Initial Commit

---
+++ SwiftUIImageColorPicker/SwiftUIImageColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIImageColorPickerApp.swift
+//  SwiftUIImageColorPicker
+//
+//  Created by Kaori Persson on 2022-11-19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIImageColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
